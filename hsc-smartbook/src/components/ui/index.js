3f03771652ba40e6732e1b10e7a5025e83ccaae0
Kunal Sharma <kunal.lalbahadur.sharma05@gmail.com>
2026-03-25 09:24:22 +0530

    Index js

--- hsc-smartbook/src/components/ui/index.js
+++ hsc-smartbook/src/components/ui/index.js
@@ -1,5 +1,2 @@
-export { default as Badge } from './Badge';
-export { default as Button } from './Button';
-export { default as Field } from './Field';
-export { ResultBox, StepList, InfoBox, MonoBlock } from './Display';
-export { default as SimCard } from './SimCard';
+export { default as Sidebar } from './Sidebar';
+export { default as Topbar } from './Topbar';
